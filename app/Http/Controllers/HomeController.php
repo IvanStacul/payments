05b2fc04b04c1661f47118acf7c82b5f481ffe6c
Ivan Stacul <stacul.ivan98@gmail.com>
2021-02-08 16:12:35 -0300

    agregado el formuario para procesar pagos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Currency;
+use App\Models\PaymentPlatform;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function index()
+	{
+		$currencies = Currency::all();
+		$paymentsPlatform = PaymentPlatform::all();
+		return view('home', compact('currencies','paymentsPlatform'));
+	}
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,54 @@
 				<div class="card-header">{{ __('Dashboard') }}</div>
 
 				<div class="card-body">
-					@if (session('status'))
-					<div class="alert alert-success" role="alert">
-						{{ session('status') }}
-					</div>
-					@endif
+					<form action="#" method="POST" id="paymentForm">
+						@csrf
+						<div class="row">
+							<div class="col-auto">
+								<div class="form-group">
+									<label for="value">{{ __('How much you want pay?') }}</label>
+									<input type="number" min="5" step="0.01" value="{{ mt_rand(500, 100000)/100 }}" class="form-control"
+										name="value" id="value" aria-describedby="helpValue" required>
+									<small id="helpValue" class="form-text text-muted">Debe ingresar un monto correcto.</small>
+								</div>
+							</div>
+							<div class="col-auto">
+								<div class="form-group">
+									<label for="currency">{{ __('Currencies') }}</label>
+									<select class="form-control" name="currency" id="currency" required>
+										@foreach ($currencies as $currency)
+										<option value="{{ $currency->iso }}">{{ strtoupper($currency->iso) }}</option>
+										@endforeach
+									</select>
+								</div>
+							</div>
+						</div>
 
-					{{ __('You are logged in!') }}
+						<div class="row mt-3">
+							<div class="form-group" id="toggler">
+								<div class="col">
+									<p> {{ __('Select the desired payment platfrom:') }} </p>
+								</div>
+								<div class="btn-group btn-group-toggle" data-toggle="buttons">
+									@foreach ($paymentsPlatform as $paymentPlatform)
+									<label class="btn btn-outline-secondary rounded m-2 p-1" data-toggle="collapse" data-target="#{{ $paymentPlatform->name }}">
+										<input type="radio" name="payment_platform" value="{{$paymentPlatform->id}}" required>
+										<img src="{{ asset($paymentPlatform->image) }}" class="img-thumbnail">
+									</label>
+									@endforeach
+								</div>
+								@foreach ($paymentsPlatform as $paymentPlatform)
+								<div id="{{ $paymentPlatform->name }}" class="collapse" data-parent="#toggler">
+									@includeIf('components.' . strtolower($paymentPlatform->name))
+								</div>
+								@endforeach
+							</div>
+						</div>
+
+						<div class="mt-3">
+							<button type="submit" id="payButton" class="btn btn-primary">{{ __('Pay') }}</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
